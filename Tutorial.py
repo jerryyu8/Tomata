9356e88b14b0d691abaeb0a540f1608c623acc18
Jerry Yu <jerryyu8@gmail.com>
2017-01-10 22:06:55 -0800

    Added sigmoid/derivative

--- Tutorial.py
+++ Tutorial.py
@@ -3,9 +3,15 @@ import numpy as np
 # Tutorial from
 # http://iamtrask.github.io/2015/07/12/basic-python-network/
 
+# sigmoid function
+# translates from numbers to range of -1 to 1
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
 
-#sigmoid function
-def nonlin(x, deriv=False):
-    if ()
+# deriv function of sigmoid (tells confidence)
+# gets derivative of the point from range of -1 to 1
+def deriv(y):
+    return x * (1-x)
+
 
 
