5a9e7326c601f0eee70b635e82116d723c728af7
Jerry Yu <jerryyu8@gmail.com>
2017-01-12 01:06:14 -0800

    Red Image Processing

--- ImageTest.py
+++ ImageTest.py
@@ -9,6 +9,8 @@ class Game(object):
         self.initVegPics()
         self.initTomPics()
         self.allPics = self.vegetablePics + self.tomatoPics
+        self.input = []
+        self.print = True
 
     def initVegPics(self):
         mainPath = os.getcwd()
@@ -24,10 +26,29 @@ class Game(object):
             self.tomatoPics.append(cv2.imread(fileName))
         os.chdir(mainPath)
 
+    def convertToInputPic(self, source):
+        # Convert to HSV
+        source = cv2.cvtColor(source,cv2.COLOR_BGR2HSV)
+        # Convert to isolate red
+        if self.print:
+            print(source[0][0])
+            self.print = False
+        inputPic = []
+        for row in range(len(source)):
+            newRow = []
+            for col in range(len(source[0])):
+                hue = source[row][col][0]
+                if hue < 18 or hue > 340: newRow.append(1)
+                else: newRow.append(0)
+            inputPic.append(newRow)
+        return inputPic
+
     def run(self):
         self.y = np.array([[0,0,0,0,0,1,1,1,1,1]]).T
-        print(self.allPics[0])
-        print('run')
 
+        for i in range(len(self.allPics)):
+            print(len(self.allPics[i]), len(self.allPics[i][0]))
+            self.input.append(self.convertToInputPic(self.allPics[i]))
+        print(self.input[0][0])
 game = Game()
 game.run()
--- ImageURLTest.py
+++ ImageURLTest.py
@@ -1,8 +1,6 @@
 import requests
 
 sources = open("TomatoURLs.txt",'r')
-print(sources.read())
-
-img_data = requests.get("http://farm3.static.flickr.com/2370/2054573289_62c4e4b029.jpg").content
+img_data = requests.get("http://farm1.static.flickr.com/211/456466127_6bed792d6f.jpg").content
 with open('image_name.jpg', 'wb') as handler:
     handler.write(img_data)
